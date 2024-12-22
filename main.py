04ab7adc5a62e1a1d7563f3f36ea345e4dd54ecc
root <root@4pfg.l.time4vps.cloud>
2024-12-22 15:44:52 +0200

    del main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import subprocess
 
-subprocess.run(['python3', 'enaza.py'])
+# Run Uvicorn in the background
+uvicorn_process = subprocess.Popen(["uvicorn", "api_keybin_enaza:app", "--reload"])
+
+# Run enaza.py after Uvicorn starts
+enaza_process = subprocess.Popen(["python3", "enaza.py", "enaza_k.py"])
+
+# Optionally, wait for both processes to finish (if needed)
+uvicorn_process.wait()
+enaza_process.wait()
 
